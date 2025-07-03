e760085afb9329b5e95ca91c1d6529662b4bf2b3
MinhCong0308 <minh.nc226056@sis.hust.edu.vn>
2025-07-03 17:11:51 +0700

    Some add for sign up page

--- blogapp/src/app/pages/auth/auth-module.ts
+++ blogapp/src/app/pages/auth/auth-module.ts
@@ -4,14 +4,15 @@ import { CommonModule } from '@angular/common';
 import { Login } from './login/login';    
 import { RouterModule } from '@angular/router';
 import { authRoutes } from './auth_routes';
-
+import { Register } from './register/register';
 
 
 @NgModule({
-  declarations: [Login],
+  declarations: [Login, Register],
   imports: [
     CommonModule,
     FormsModule,
+    RouterModule, // Import RouterModule for routing
     RouterModule.forChild(authRoutes) // Register auth routes
   ],
 })

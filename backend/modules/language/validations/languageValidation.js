a53fef7eb2314ebea8b11930adf8ad8732b3c998
SonPham <phamcongson297@gmail.com>
2025-07-18 13:12:49 +0700

    Thêm check unique cho languageValidation, Thêm js thêm language trong trang admin-languages

--- backend/modules/language/validations/languageValidation.js
+++ backend/modules/language/validations/languageValidation.js
@@ -1,12 +1,14 @@
 const { BodyWithLocale } = require("kernels/rules");
+const db = require("models/index");
+const language = db.Language;
 
 const languageValidation = {
     create: [
-        new BodyWithLocale("languagename").notEmpty().isString(),
-        new BodyWithLocale("locale_code").notEmpty().isString()
+        new BodyWithLocale("languagename").notEmpty().isString().unique(language, "languagename"),
+        new BodyWithLocale("locale_code").notEmpty().isString().unique(language, "locale_code")
     ],
-    update: [new BodyWithLocale("languagename").notEmpty().isString(),
-    new BodyWithLocale("locale_code").notEmpty().isString()
+    update: [new BodyWithLocale("languagename").notEmpty().isString().unique(language, "languagename"),
+    new BodyWithLocale("locale_code").notEmpty().isString().unique(language, "locale_code")
     ]
 }
 module.exports = languageValidation;

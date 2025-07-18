533ba1d64320135eba4830256d6bef3ba9b91e52
SonPham <phamcongson297@gmail.com>
2025-07-18 11:21:54 +0700

    Thêm check unique cho categoryValidation

--- backend/modules/category/validations/categoryValidation.js
+++ backend/modules/category/validations/categoryValidation.js
@@ -1,11 +1,13 @@
 const {BodyWithLocale} = require("kernels/rules");
+const db = require("models/index");
+const category = db.Category;
 
 const categoryValidation = {
     create: [
-        new BodyWithLocale("categoryname").notEmpty().isString()
+        new BodyWithLocale("categoryname").notEmpty().isString().unique(category, "categoryname")
     ],
     update: [
-        new BodyWithLocale("categoryname").notEmpty().isString()
+        new BodyWithLocale("categoryname").notEmpty().isString().unique(category, "categoryname")
     ]
 };
 

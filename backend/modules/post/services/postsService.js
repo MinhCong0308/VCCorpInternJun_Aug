5db1dd54a801ebb50367a1d7b25679eb8cd79b1f
SonPham <phamcongson297@gmail.com>
2025-08-03 15:56:27 +0700

    Add API GET posts sort by trending, API PUT like/unlike post

--- backend/modules/post/services/postsService.js
+++ backend/modules/post/services/postsService.js
@@ -59,6 +59,62 @@ const postsService = {
             totalPages: Math.ceil(count / limit)
         };
     },
+    getPublishedPostsTrending: async (categoryId, userId, languageId, limit = 5, page = 1, search = '') => {
+        const offset = (page - 1) * limit;
+
+        const options = {
+            limit,
+            offset,
+            order: [['like_cnt', 'DESC']],
+            where: { status: 2 }
+        };
+
+        if (search && search.trim() !== '') {
+            options.where = {
+                [Sequelize.Op.and]: [
+                    { status: 2 },
+                    Sequelize.literal(`MATCH(title, content) AGAINST('${search.trim()}' IN NATURAL LANGUAGE MODE)`)
+                ]
+            };
+        };
+        if (userId) {
+            options.where = { ...options.where, userId };
+        };
+        if (languageId) {
+            options.where = { ...options.where, languageId };
+        };
+        let categoryInclude = {
+            model: db.Category,
+            as: 'Categories',
+            attributes: ['categoryid', 'categoryname'],
+            through: { attributes: [] }
+        };
+        if (categoryId) {
+            categoryInclude.where = { categoryid: categoryId };
+            categoryInclude.required = true;
+        }
+
+        const { count, rows } = await db.Post.findAndCountAll({
+            ...options,
+            include: [
+                {model: db.User, attributes: ['firstname', 'lastname', 'avatar']},
+                {model: db.Language, attributes: ['languagename']},
+                categoryInclude
+            ],
+            distinct: true
+        });
+
+        return {
+            posts: rows.map(post => {
+                const postJSON = post.toJSON();
+                postJSON.coverImage = extractCoverImage(postJSON.content);
+                return postJSON;
+            }),
+            total: count,
+            page,
+            totalPages: Math.ceil(count / limit)
+        };
+    },
     getPublishedPostDetail: async (postId) => {
         const post = await db.Post.findOne({
             where: { 
@@ -77,6 +133,27 @@ const postsService = {
         const postJSON = post.toJSON();
         postJSON.coverImage = extractCoverImage(postJSON.content);
         return postJSON;
+    },
+    likePost: async (postId) => {
+        const post = await db.Post.findByPk(postId);
+        if (!post) {
+            throw new Error("Post not found");
+        }
+        post.like_cnt += 1;
+        await post.save();
+        return post.toJSON();
+    },
+    unlikePost: async (postId) => {
+        const post = await db.Post.findByPk(postId);
+        if (!post) {
+            throw new Error("Post not found");
+        }
+        if (post.like_cnt <= 0) {
+            throw new Error("This post has no likes");
+        }
+        post.like_cnt -= 1;
+        await post.save();
+        return post.toJSON();
     }
 };
 

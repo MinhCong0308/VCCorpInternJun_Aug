5fabab5b1bcc23046f3ec2b2f9481d446e9268f1
SonPham <phamcongson297@gmail.com>
2025-08-15 17:02:51 +0700

    Fix comment post detail page angular

--- frontend/src/app/post/post.module.ts
+++ frontend/src/app/post/post.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostDetailComponent } from './post-detail/post-detail.component';
 import { PostRoutingModule } from './post-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [PostDetailComponent],
   imports: [
     CommonModule,
-    PostRoutingModule
+    PostRoutingModule,
+    FormsModule
   ]
 })
 export class PostModule { }
